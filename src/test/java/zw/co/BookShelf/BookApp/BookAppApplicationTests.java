bd2056d7336394d8e4cf9e545ef1f3dc951cbacd
KudakwasheMurungweni <kudakwashemurungweni50@gmail.com>
2025-07-09 22:01:24 +0200

    Project setup

---
+++ src/test/java/zw/co/BookShelf/BookApp/BookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.BookShelf.BookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
